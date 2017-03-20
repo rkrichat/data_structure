541da7bb75ff1d28e9b9f808e0277e4656bd3e07
rkritchat <rkritchat@scan-it.com.sg>
2017-03-20 21:49:30 +0700

    Create Doubly Link List

---
+++ src/doublylinklist/doublelink/DoubleLink.java
@@ -0,0 +1,45 @@
+package doublylinklist.doublelink;
+/**
+ * This class for prepare Doubly Link List. :: 20/03/2017
+ * @author NP
+ *
+ */
+public class DoubleLink {
+	private DoubleLink next = null;;
+	private DoubleLink precede = null;;
+	private Object item = null;;
+	
+	public DoubleLink(Object item){
+		this.item = item;
+		this.precede = null;
+		this.next = null;
+	}
+	
+	public DoubleLink(Object item, DoubleLink precede, DoubleLink next){
+		this.item = item;
+		this.precede = precede;
+		this.next = next;
+	}
+	
+	//getter
+	public DoubleLink getNext() {
+		return next;
+	}
+	public DoubleLink getPrecede() {
+		return precede;
+	}
+	public Object getItem() {
+		return item;
+	}
+	
+	//setter
+	public void setNext(DoubleLink next) {
+		this.next = next;
+	}
+	public void setPrecede(DoubleLink precede) {
+		this.precede = precede;
+	}
+	public void setItem(Object item) {
+		this.item = item;
+	}
+}
